35c11b71c761364b1b2886128af552c4149529f4
mahlafadaei2021@gmail.com <mahlafadaei2021@gmail.com>
2021-10-28 17:56:10 +0330

    added ex3

---
+++ ex3-hijackLogo.js
@@ -0,0 +1,8 @@
+function changeLogo (){
+
+    const googleLogo = document.getElementsByClassName('lnXdpd');
+    googleLogo.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
+    googleLogo.scrset = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
+};
+
+changeLogo();
